355123a345bc4a18ec86c6d892b70526d891636d
PIDTuningIreland <128464082+PIDTuningIreland@users.noreply.github.com>
2024-06-28 13:51:43 +0100

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def parse_requirements(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+setup(
+    name="pypidtune",
+    version="0.0.01",
+    author="PIDTuningIreland",
+    author_email="pidtuningireland@gmail.com",
+    description="PID tuner, logger, simulator and process emulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/PIDTuningIreland/pypidtune",
+    keywords="PID Tuner",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=parse_requirements("requirements.txt"),
+)
