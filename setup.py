aa3d11d40aa577f0e025e269a9ca763abd94a715
PIDTuningIreland <128464082+PIDTuningIreland@users.noreply.github.com>
2024-07-09 10:57:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 import codecs
-import urllib.request
-import json
 
 
 def parse_requirements(filename):
@@ -9,23 +7,12 @@ def parse_requirements(filename):
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 
-def get_latest_release_version():
-    url = f"https://api.github.com/repos/PIDTuningIreland/pyPIDTune/releases/latest"
-    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
-    response = urllib.request.urlopen(req)
-    if response.getcode() == 200:
-        release_info = json.loads(response.read())
-        return release_info["tag_name"]
-    else:
-        raise Exception("Cannot Find Version")
-
-
 with codecs.open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="pypidtune",
-    version=get_latest_release_version(),
+    version="{{VERSION_PLACEHOLDER}}",
     license="MIT",
     license_files=("LICENSE",),
     author="PIDTuningIreland",
